494414f980d12a6509e601a5f82bf65ff5e67899
leumasmelo <142456194+leumasmelo@users.noreply.github.com>
2023-08-22 21:45:56 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,20 @@
+/*
+*Samuel Melo
+*
+*/
+
+//1. 10; 10,5
+//2. 20, 20, 10
+
+//4.
+
+// exercicio 3
+
+let nome;
+let idade;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+nome = prompt("Qual é o seu nome?");
+idade = prompt ("Qual a sua idade?");
